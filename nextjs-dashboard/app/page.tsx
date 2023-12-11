934a4deb3f8e9293874f3980e979393e1b4a7960
unknown <b7industry@gmail.com>
2023-12-11 20:39:50 +0800

    updated page.tsx

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -4,7 +4,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
-
+ 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
